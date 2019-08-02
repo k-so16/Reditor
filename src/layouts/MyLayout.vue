e4b45cb0bde90079cb509239681a2c1903104fc4
Soichiro KATO <kato-soichiro@msen.jp>
2019-08-02 16:49:28 +0900

    added q-header tag

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,24 +1,26 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-bar class="q-electron-drag bg-black text-white">
-      <div class="cursor-pointer q-electron-drag--exception">
-        File
-        <q-menu>
-          <q-list dense>
-            <q-item clickable v-close-popup @click="save">
-              <q-item-section>Save</q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup @click="close">
-              <q-item-section>Quit</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </div>
-      <q-space />
-      <q-btn dense flat icon="minimize" @click="minimize" />
-      <q-btn dense flat icon="crop_square" @click="maximize" />
-      <q-btn dense flat icon="close" @click="close" />
-    </q-bar>
+    <q-header elevated>
+      <q-bar class="q-electron-drag bg-black text-white">
+        <div class="cursor-pointer q-electron-drag--exception">
+          File
+          <q-menu>
+            <q-list dense>
+              <q-item clickable v-close-popup @click="save">
+                <q-item-section>Save</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="close">
+                <q-item-section>Quit</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+        <q-space />
+        <q-btn dense flat icon="minimize" @click="minimize" />
+        <q-btn dense flat icon="crop_square" @click="maximize" />
+        <q-btn dense flat icon="close" @click="close" />
+      </q-bar>
+    </q-header>
     <q-page-container>
       <router-view :file.sync="file" />
     </q-page-container>
